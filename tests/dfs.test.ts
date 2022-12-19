9b6c77c6a65ae8be076972453681405386478678
yactouat <yactouat@hotmail.com>
2022-12-19 17:30:44 +0100

    initial dfs commit

---
+++ tests/dfs.test.ts
@@ -0,0 +1,9 @@
+// TODO
+describe('testing `DFSGraph.dfs`', () => {
+
+    // this test is just here so jest does not complain
+    test('fake test', () => {
+        expect(true).toBe(true);
+    });
+
+});
