7449d07a7c81232ec8de036aa149c503787e5e74
yactouat <yactouat@hotmail.com>
2023-01-01 12:59:09 +0100

    TODO graph V2

---
+++ src/dfs/graph-v2.ts
@@ -0,0 +1,53 @@
+export class Graph<T> {
+
+    // TODO: populate the graph at construction time + add a method to add an edge
+
+    // TODO: each vertex should be identified with a unique number
+
+    // TODO: denote the vertex set V and the edge set E
+
+    // TODO: implement a simple edge list like so => [ [0,1], [0,6], [0,8], [1,4], [1,6], [1,9], [2,4], [2,6], [3,4], [3,5], [3,8], [4,5], [4,9], [7,8], [7,9] ]
+
+    // TODO: implement an adjacency matrix, an adjacency matrix can be represented like so => 
+        /**
+         * [ [0, 1, 0, 0, 0, 0, 1, 0, 1, 0],
+         *   [1, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+         *   [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+         *   [0, 0, 0, 0, 1, 1, 0, 0, 1, 0],
+         *   [0, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+         *   [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+         *   [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+         *   [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+         *   [1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+         *   [0, 1, 0, 0, 1, 0, 0, 1, 0, 0] ]
+         */
+
+    // TODO: implement adjacency list as an array of arrays
+    
+    // TODO: implement adjacency list as an array of linked lists
+
+    // TODO: experiment with a social network graph
+
+    // TODO: determine if a given edge is in the graph
+
+    // TODO: find all the neighbors of a vertex
+
+    // TODO: UI to visualize the graph
+
+    // TODO: represent 'the number of edges is the degree of the vertex'
+
+    // TODO: get the shortest path between two vertices
+
+    // TODO: add method to scan a graph for existing cycles
+
+    // TODO: implement weights for edges
+
+    // TODO: experiment with a roadmap of cities/distance between them + find the shortest path in the weighted graph
+
+    // TODO: implement directed graphs
+
+    // TODO: find strongly connected components in a directed graph
+
+    // TODO: model out and in degress of a vertex in a directed graph
+
+}
